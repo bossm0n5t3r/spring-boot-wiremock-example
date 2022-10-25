f19686822e804ba2c9ed056997eea774ccb0fda5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-10-26 05:17:10 +0900

    fix: Edit SpringBootWiremockExampleApplication
    - Add ConfigurationPropertiesScan annotation

--- src/main/kotlin/me/bossm0n5t3r/wiremock/SpringBootWiremockExampleApplication.kt
+++ src/main/kotlin/me/bossm0n5t3r/wiremock/SpringBootWiremockExampleApplication.kt
@@ -1,9 +1,11 @@
 package me.bossm0n5t3r.wiremock
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class SpringBootWiremockExampleApplication
 
 fun main(args: Array<String>) {
