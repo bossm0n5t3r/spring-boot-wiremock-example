4434e293a8c64faeda82c7a863f939a8b14c59b9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-10-28 06:53:20 +0900

    feat: Add FakeStoreProperties

---
+++ src/main/kotlin/me/bossm0n5t3r/wiremock/properties/FakeStoreProperties.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.wiremock.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("fake-store")
+data class FakeStoreProperties(
+    @JvmField
+    val api: String
+)
