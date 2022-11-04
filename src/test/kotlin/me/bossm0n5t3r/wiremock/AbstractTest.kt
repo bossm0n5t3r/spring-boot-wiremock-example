5cf8c039e418a18fd741bdb2137e34d3efb147db
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-05 05:43:10 +0900

    test: Add AbstractTest

---
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/AbstractTest.kt
@@ -0,0 +1,8 @@
+package me.bossm0n5t3r.wiremock
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+abstract class AbstractTest
