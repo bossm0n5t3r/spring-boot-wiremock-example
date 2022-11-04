30b7b6ef2f744beb4efd6ef569ef60903eb95a1b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-05 05:44:41 +0900

    test: Edit ApplicationTests
    - extends AbstractTest

--- src/test/kotlin/me/bossm0n5t3r/wiremock/SpringBootWiremockExampleApplicationTests.kt
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/SpringBootWiremockExampleApplicationTests.kt
@@ -1,11 +1,8 @@
 package me.bossm0n5t3r.wiremock
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class SpringBootWiremockExampleApplicationTests {
-
+class SpringBootWiremockExampleApplicationTests : AbstractTest() {
     @Test
     fun contextLoads() {
     }
