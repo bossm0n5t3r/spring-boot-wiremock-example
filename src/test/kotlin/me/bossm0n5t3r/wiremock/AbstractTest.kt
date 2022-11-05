8b4ffa9db2b711f63441feb16b45efcf1a51dda6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-06 08:55:50 +0900

    test: Delete AbstractTest

--- src/test/kotlin/me/bossm0n5t3r/wiremock/AbstractTest.kt
+++
@@ -1,8 +0,0 @@
-package me.bossm0n5t3r.wiremock
-
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-
-@SpringBootTest
-@ActiveProfiles("test")
-abstract class AbstractTest
