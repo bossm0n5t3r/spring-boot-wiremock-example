816647f75c4fcd38736e6839ce54e2353c592432
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-06 09:10:54 +0900

    test: Add DummyServiceWithNonStaticPropertiesTest
    - Add "getAllProductsTest" using WireMockServer

---
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/application/DummyServiceWithNonStaticPropertiesTest.kt
@@ -0,0 +1,52 @@
+package me.bossm0n5t3r.wiremock.application
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.exactly
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.ok
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.junit5.WireMockTest
+import io.mockk.every
+import io.mockk.mockk
+import me.bossm0n5t3r.wiremock.properties.FakeStoreProperties
+import me.bossm0n5t3r.wiremock.util.ResourceUtil.readFileAsJson
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.web.client.RestTemplate
+
+@WireMockTest
+internal class DummyServiceWithNonStaticPropertiesTest {
+    private val fakeStoreWiremockServer = WireMockServer(WireMockConfiguration.options().dynamicPort())
+
+    init {
+        fakeStoreWiremockServer.start()
+    }
+
+    private val restTemplate = RestTemplate()
+    private val objectMapper = jacksonObjectMapper()
+    private val fakeStoreProperties = mockk<FakeStoreProperties>()
+    private val sut = DummyServiceWithNonStaticProperties(
+        restTemplate,
+        objectMapper,
+        fakeStoreProperties
+    )
+
+    @Test
+    fun getAllProductsTest() {
+        fakeStoreWiremockServer.stubFor(
+            get(urlPathEqualTo("/products"))
+                .willReturn(ok().withBody("products.json".readFileAsJson()))
+        )
+        every { fakeStoreProperties.api } returns fakeStoreWiremockServer.baseUrl()
+
+        val result = sut.getAllProducts()
+
+        assertThat(result).isNotEmpty
+        fakeStoreWiremockServer.verify(exactly(1), getRequestedFor(urlEqualTo("/products")))
+        fakeStoreWiremockServer.stop()
+    }
+}
