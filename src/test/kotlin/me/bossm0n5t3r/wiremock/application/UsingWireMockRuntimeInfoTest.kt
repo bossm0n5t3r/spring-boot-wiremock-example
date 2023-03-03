35f14b58d33cbd40ded8187b3988839de5979b52
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-03-03 22:50:10 +0900

    test: Add UsingWireMockRuntimeInfoTest

---
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/application/UsingWireMockRuntimeInfoTest.kt
@@ -0,0 +1,46 @@
+package me.bossm0n5t3r.wiremock.application
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.tomakehurst.wiremock.client.WireMock.exactly
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.ok
+import com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.verify
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo
+import com.github.tomakehurst.wiremock.junit5.WireMockTest
+import io.mockk.every
+import io.mockk.mockk
+import me.bossm0n5t3r.wiremock.properties.FakeStoreProperties
+import me.bossm0n5t3r.wiremock.util.ResourceUtil.readFileAsJson
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.web.client.RestTemplate
+
+@WireMockTest
+internal class UsingWireMockRuntimeInfoTest {
+    private val restTemplate = RestTemplate()
+    private val objectMapper = jacksonObjectMapper()
+    private val fakeStoreProperties = mockk<FakeStoreProperties>()
+    private val sut = DummyServiceWithNonStaticProperties(
+        restTemplate,
+        objectMapper,
+        fakeStoreProperties
+    )
+
+    @Test
+    fun getAllProductsTest(wiremockRuntimeInfo: WireMockRuntimeInfo) {
+        stubFor(
+            get(urlPathEqualTo("/products"))
+                .willReturn(ok().withBody("products.json".readFileAsJson()))
+        )
+        every { fakeStoreProperties.api } returns wiremockRuntimeInfo.httpBaseUrl
+
+        val result = sut.getAllProducts()
+
+        assertThat(result).isNotEmpty
+        verify(exactly(1), getRequestedFor(urlEqualTo("/products")))
+    }
+}
