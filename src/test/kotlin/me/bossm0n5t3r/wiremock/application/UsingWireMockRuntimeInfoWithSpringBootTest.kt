eeb8d8716d392734b62367f64d335d2af73f5135
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-03-03 22:49:20 +0900

    fix: Rename to UsingWireMockRuntimeInfoWithSpringBootTest

--- src/test/kotlin/me/bossm0n5t3r/wiremock/application/UsingWireMockRuntimeInfoWithSpringBootTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/application/UsingWireMockRuntimeInfoWithSpringBootTest.kt
@@ -1,14 +1,15 @@
 package me.bossm0n5t3r.wiremock.application
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.exactly
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.ok
+import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.client.WireMock.verify
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo
 import com.github.tomakehurst.wiremock.junit5.WireMockTest
 import io.mockk.every
 import io.mockk.mockk
@@ -16,18 +17,15 @@ import me.bossm0n5t3r.wiremock.common.AbstractSpringBootTest
 import me.bossm0n5t3r.wiremock.properties.FakeStoreProperties
 import me.bossm0n5t3r.wiremock.util.ResourceUtil.readFileAsJson
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.client.RestTemplate
 
 @WireMockTest
-internal class DummyServiceWithNonStaticPropertiesWithSpringBootTest @Autowired private constructor(
+internal class UsingWireMockRuntimeInfoWithSpringBootTest @Autowired private constructor(
     restTemplate: RestTemplate,
     objectMapper: ObjectMapper,
 ) : AbstractSpringBootTest() {
-    private val fakeStoreWiremockServer = WireMockServer(WireMockConfiguration.options().dynamicPort())
     private val fakeStoreProperties = mockk<FakeStoreProperties>()
     private val sut = DummyServiceWithNonStaticProperties(
         restTemplate,
@@ -35,28 +33,17 @@ internal class DummyServiceWithNonStaticPropertiesWithSpringBootTest @Autowired 
         fakeStoreProperties
     )
 
-    @BeforeEach
-    fun setup() {
-        fakeStoreWiremockServer.start()
-    }
-
-    @AfterEach
-    fun cleanUp() {
-        fakeStoreWiremockServer.stop()
-    }
-
     @Test
-    fun getAllProductsTest() {
-        fakeStoreWiremockServer.stubFor(
+    fun getAllProductsTest(wiremockRuntimeInfo: WireMockRuntimeInfo) {
+        stubFor(
             get(urlPathEqualTo("/products"))
                 .willReturn(ok().withBody("products.json".readFileAsJson()))
         )
-        every { fakeStoreProperties.api } returns fakeStoreWiremockServer.baseUrl()
+        every { fakeStoreProperties.api } returns wiremockRuntimeInfo.httpBaseUrl
 
         val result = sut.getAllProducts()
 
         assertThat(result).isNotEmpty
-        fakeStoreWiremockServer.verify(exactly(1), getRequestedFor(urlEqualTo("/products")))
-        fakeStoreWiremockServer.stop()
+        verify(exactly(1), getRequestedFor(urlEqualTo("/products")))
     }
 }
