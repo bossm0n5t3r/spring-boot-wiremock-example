3f90e49cbfe61371416ab694fc3d8ed47e1919ee
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-03-03 22:30:01 +0900

    feat: Add and apply AbstractSpringBootTest
    - Add AbstractSpringBootTest
    - Apply AbstractSpringBootTest
      - Edit SpringBootWiremockExampleApplicationTests

--- src/test/kotlin/me/bossm0n5t3r/wiremock/SpringBootWiremockExampleApplicationTests.kt
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/SpringBootWiremockExampleApplicationTests.kt
@@ -1,10 +1,9 @@
 package me.bossm0n5t3r.wiremock
 
+import me.bossm0n5t3r.wiremock.common.AbstractSpringBootTest
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class SpringBootWiremockExampleApplicationTests {
+class SpringBootWiremockExampleApplicationTests : AbstractSpringBootTest() {
     @Test
     fun contextLoads() {
     }
---
+++ src/test/kotlin/me/bossm0n5t3r/wiremock/common/AbstractSpringBootTest.kt
@@ -0,0 +1,6 @@
+package me.bossm0n5t3r.wiremock.common
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+abstract class AbstractSpringBootTest
